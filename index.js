f437a93ef615fb22d807dbf2cfe96687e9292376
Roy Peters <roy@aanzee.nl>
2015-10-07 15:42:16 +0200

    Implemented a win32 diskspace package.

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ exports.isMonitoringModule = true;
 exports.hasCron = true;
 exports.snapshotData = true;
 
-var njds = require('nodejs-disks');
-var responseMessaging = require('monitor-response');
+var os = require('os');
+var njds = null;
+var diskinfo = null;
+
+try {
+	njds = require('nodejs-disks');
+} catch(e) {}
+try {
+	diskinfo = require('diskinfo');
+} catch(e) {}
 
 exports.getRoutes = function () {
 	return [
@@ -12,49 +20,63 @@ exports.getRoutes = function () {
 }
 
 exports.executeCron = function (callback) {
-    diskspaceData(function(err, data){
-        if(err)
-            callback(err);
-        else
-            callback(null, data);
-    });
+	diskspaceData(function(err, data){
+		if(err)
+			callback(err);
+		else
+			callback(null, data);
+	});
 }
 
 var diskspaceData = function(callback){
-    njds.drives(
-        function (err, drives) {
-            njds.drivesDetail(
-                drives,
-                function (err, data) {
-                    // for(var i = 0; i<data.length; i++)
-                    // {
-                    //     console.log(data[i].mountpoint); /* Get drive mount point */
-                    //     console.log(data[i].total); /* Get drive total space */
-                    //     console.log(data[i].used); /* Get drive used space */
-                    //     console.log(data[i].available); /* Get drive available space  */
-                    //     console.log(data[i].drive); /* Get drive name */
-                    //     console.log(data[i].usedPer); /* Get drive used percentage */
-                    //     console.log(data[i].freePer); /* Get drive free percentage */
-                    // }
+	switch (os.platform().toLowerCase()) {
+		case 'win32':
+			win32DiskspaceData(callback);
+			break;
+		
+		case 'linux':
+			linuxDarwinDiskspaceData(callback);
+			break;
 
-                    if(err)
-                        callback(err);
-
-                    callback(null, data);
-
-                }
-            );
-        }
-    )
+		case 'darwin':
+			linuxDarwinDiskspaceData(callback);
+			break;
+	}
+	
 }
 
-var getDiskspace = function(req, res, next) {
-
-    diskspaceData(function(err, data){
-        if(err)
-            res.json(responseMessaging.format(500, {}, [err]));
-        else
-            res.json(responseMessaging.format(200, data));  
-    });
-
+var linuxDarwinDiskspaceData = function(callback) {
+	njds.drives(
+		function (err, drives) {
+			njds.drivesDetail(
+				drives,
+				function (err, data) {
+					if(err)
+						callback(err);
+					else
+						callback(null, data); // TODO: Change data to readable bytes
+				}
+			);
+		}
+	)
 }
+
+var win32DiskspaceData = function(callback) {
+	diskinfo.getDrives(function(err, aDrives) {
+		if(err)
+			callback(err);
+		else
+			callback(null, data); // TODO: Change data structure and data to readable bytes
+	});
+}
+
+// var getDiskspace = function(req, res, next) {
+
+//  diskspaceData(function(err, data){
+//      if(err)
+//          res.json(responseMessaging.format(500, {}, [err]));
+//      else
+//          res.json(responseMessaging.format(200, data));  
+//  });
+
+// }
